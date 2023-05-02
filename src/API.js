aa2060ef71418464c6371932c11f0123d0ef1f8b
Filipe Mendonça <filipe.m.almeida@gmail.com>
2023-05-02 17:41:52 -0300

    teste de api

---
+++ src/API.js
@@ -0,0 +1,14 @@
+export const API_URL = "https://eventozen-api.azurewebsites.net/";
+
+export function USER_POST(body) {
+  return {
+    url: API_URL + "/api/identity/users",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { USER_POST } from "./API";
 
 const App = () => {
-  return <div>App</div>;
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      userName: event.target[0].value,
+      name: event.target[1].value,
+      surname: event.target[2].value,
+      email: event.target[3].value,
+      phoneNumber: event.target[4].value,
+      isActive: true,
+      lockoutEnabled: true,
+      roleNames: ["string"],
+      password: event.target[5].value,
+    });
+    const fetchh = await fetch(url, options);
+
+    console.log(fetchh);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label style={{ display: "block" }} htmlFor="username">
+        Nome de Usuário
+      </label>
+      <input style={{ display: "block" }} type="text" name="username" />
+
+      <label style={{ display: "block" }} htmlFor="appName">
+        Nome
+      </label>
+      <input style={{ display: "block" }} type="text" name="appName" />
+
+      <label style={{ display: "block" }} htmlFor="surName">
+        Sobrenome
+      </label>
+      <input style={{ display: "block" }} type="text" name="surName" />
+
+      <label style={{ display: "block" }} htmlFor="email">
+        Email
+      </label>
+      <input style={{ display: "block" }} type="text" name="email" />
+
+      <label style={{ display: "block" }} htmlFor="phoneNumber">
+        Telefone
+      </label>
+      <input style={{ display: "block" }} type="text" name="phoneNumber" />
+
+      <label style={{ display: "block" }} htmlFor="password">
+        Senha
+      </label>
+      <input style={{ display: "block" }} type="password" name="password" />
+      <button>Enviar</button>
+    </form>
+  );
 };
 
 export default App;
